3579f35d4a4cd4c457b0b692c74a4dbb79d427b0
Chae-yun <ycy6685@gmail.com>
2017-09-20 21:55:57 +0900

    Print List of Searched Tweets

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -1,7 +1,11 @@
 package kr.hs.emirim.nmb2017.springmvc4.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,8 +19,8 @@ public class HelloController {
 	@RequestMapping("/")
 	public String hello(@RequestParam(defaultValue = "내일") String search, Model model) {
 		SearchResults searchResults = twitter.searchOperations().search(search);
-		String text = searchResults.getTweets().get(0).getText(); 
-		model.addAttribute("message", text);
-		return "resultPage";
+		List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList()); 
+		model.addAttribute("tweets", tweets);
+		return "tweets";
 	}
 }
