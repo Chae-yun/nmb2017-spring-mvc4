8479ecc9c1f6bb655ba5cdcf57c3c7fe669601db
Chae-yun <ycy6685@gmail.com>
2017-09-20 20:43:48 +0900

    Send Message from Controller

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -1,12 +1,15 @@
 package kr.hs.emirim.nmb2017.springmvc4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller 
 public class HelloController {
-   @RequestMapping("/") 
-   public String hello() {
-       return "resultPage";
-   }
+
+	@RequestMapping("/")
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello from Controller!!"); 
+		return "resultPage";
+	}
 }
