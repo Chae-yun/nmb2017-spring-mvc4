d1aac4237fc6b21f36f37bf60434e64bc3b2b78c
Chae-yun <ycy6685@gmail.com>
2017-09-20 20:53:21 +0900

    Add "name" Parameter

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -3,13 +3,14 @@ package kr.hs.emirim.nmb2017.springmvc4.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller 
 public class HelloController {
 
 	@RequestMapping("/")
-	public String hello(Model model) {
-		model.addAttribute("message", "Hello from Controller!!"); 
+	public String hello(@RequestParam("name") String userName, Model model) {
+		model.addAttribute("message", userName + "님 안녕하세요~!"); 
 		return "resultPage";
 	}
 }
